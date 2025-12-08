76f2fba69b661ac8cf748061cd0f25d93cd264a7
LorenzoMinjares22 <lminjares3@miners.utep.edu>
2025-12-08 20:28:27 +0000

    finish task 3 completely

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -114,26 +114,139 @@ sys_freepmem(void)
   return pages * PGSIZE;
 }
 
+                                                                                   
+// Helper reads sem_t from user memory
+static int
+fetch_sem_index(int argn, int *idx)
+{
+  uint64 uaddr;
+  struct proc *p = myproc();
+
+  if (argaddr(argn, &uaddr) < 0)
+    return -1;
+
+  if (copyin(p->pagetable, (char *)idx, uaddr, sizeof(int)) < 0)
+    return -1;
+
+  if (*idx < 0 || *idx >= NSEM)
+    return -1;
+
+  return 0;
+}
+
+
 uint64
 sys_sem_init(void)
 {
-   return -1;
+  uint64 uaddr;   // user address of sem_t
+  int pshared;
+  int value;
+  int idx;
+  struct proc *p = myproc();
+  int ksem;      // sem_t is typedef int
+
+  // get arguments
+  if (argaddr(0, &uaddr) < 0 || argint(1, &pshared) < 0 || argint(2, &value) < 0)
+    return -1;
+
+  if (value < 0)
+    return -1;
+
+  // allocate a semaphore slot
+  idx = semalloc();
+  if (idx < 0)
+    return -1;
+
+  // initialize the semaphore's count
+  struct semaphore *s = &semtable.sem[idx];
+  acquire(&s->lock);
+  s->count = value;
+  release(&s->lock);
+
+
+  ksem = idx;
+  if (copyout(p->pagetable, uaddr, (char *)&ksem, sizeof(ksem)) < 0) {
+    semdealloc(idx);
+    return -1;
+  }
+
+  
+  return 0;
 }
 
-uint64                                                                                                                  
-sys_sem_destroy(void)                                                                                                      
-{                                                                                                                       
-   return -1;                                                                                                           
-}    
 
-uint64                                                                                                                  
-sys_sem_wait(void)                                                                                                      
-{                                                                                                                       
-   return -1;                                                                                                           
-}  
+uint64
+sys_sem_destroy(void)
+{
+  int idx;
 
-uint64                                                                                                                  
-sys_sem_post(void)                                                                                                      
-{                                                                                                                       
-   return -1;                                                                                                           
-}      
+  // read sem index from user space
+  if (fetch_sem_index(0, &idx) < 0)
+    return -1;
+
+  // mark it free in global table
+  semdealloc(idx);
+  return 0;
+}
+
+uint64
+sys_sem_wait(void)
+{
+  int idx;
+
+  if (fetch_sem_index(0, &idx) < 0)
+    return -1;
+
+  struct semaphore *s = &semtable.sem[idx];
+
+  acquire(&s->lock);
+
+  if (s->valid == 0) {
+    release(&s->lock);
+    return -1;
+  }
+
+  // wait until count > 0 sleeping while it is 0
+  while (s->count == 0) {
+    // sleep on the semaphore struc releases s->lock while sleeping
+    sleep(s, &s->lock);
+    // when woken up s->lock is re-acquired
+    if (s->valid == 0) {
+      // destroyed while sleeping
+      release(&s->lock);
+      return -1;
+    }
+  }
+
+
+  s->count--;
+  release(&s->lock);
+  return 0;
+}
+
+
+uint64
+sys_sem_post(void)
+{
+  int idx;
+
+  if (fetch_sem_index(0, &idx) < 0)
+    return -1;
+
+  struct semaphore *s = &semtable.sem[idx];
+
+  acquire(&s->lock);
+
+  if (s->valid == 0) {
+    release(&s->lock);
+    return -1;
+  }
+
+  // increment count and wake s
+  s->count++;
+  wakeup(s);   
+
+  release(&s->lock);
+  return 0;
+}
+            
